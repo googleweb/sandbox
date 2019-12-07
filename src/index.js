7ec184e722b5b26b249b6f86a8f54f6cd2b281f5
googleweb <997836850@qq.com>
2019-12-07 14:50:03 +0800

    demo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { version } from "antd";
-import "antd/dist/antd.css";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ConfigProvider, DatePicker, message } from 'antd';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
+import './index.css';
 
-ReactDOM.render(
-  <div className="App">
-    <h1>antd version: {version}</h1>
-    <p>
-      Please <b>fork</b> this sandbox to reproduce your issue.
-    </p>
-  </div>,
-  document.getElementById("root")
-);
+moment.locale('zh-cn');
+
+class App extends React.Component {
+  state = {
+    date: null,
+  };
+
+  handleChange = date => {
+    message.info(`您选择的日期是: ${date ? date.format('YYYY-MM-DD') : '未选择'}`);
+    this.setState({ date });
+  };
+  render() {
+    const { date } = this.state;
+    return (
+      <ConfigProvider locale={zhCN}>
+        <div style={{ width: 400, margin: '100px auto' }}>
+          <DatePicker onChange={this.handleChange} />
+          <div style={{ marginTop: 20 }}>
+            当前日期：{date ? date.format('YYYY-MM-DD') : '未选择'}
+          </div>
+        </div>
+      </ConfigProvider>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
